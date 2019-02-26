551865f4827d541f680de93b87f5777f46904c99
Avi407 <48006668+Avi407@users.noreply.github.com>
2019-02-26 19:14:12 +0530

    Add files via upload

---
+++ pyramid.js
@@ -0,0 +1,8 @@
+var num = "";
+var size = prompt("Enter the size of the pyramid");
+for(var i=1; i<=size; i++)
+{
+  num = num + i;
+  console.log(num+"\n");
+  //document.write("<br>"+num+"</br>");
+}
